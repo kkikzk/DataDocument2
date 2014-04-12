2c602601791d519c91589fa5a4c15562e92ef9ba
kkikzk <kkikzk@gmail.com>
2014-04-12 10:37:56 +0900

    just passed ScannerTest.testEmptyString

--- src/StringParser.rb
+++ src/StringParser.rb
@@ -1,1 +1,10 @@
 # -*- encoding: utf-8 -*-
+
+class Scanner
+  def initialize(str)
+    @str = str
+  end
+  def popToken
+    [false, false]
+  end
+end
--- test/DataDocumentTest.rb
+++ test/DataDocumentTest.rb
@@ -2,17 +2,13 @@
 require 'test/unit'
 require './ParseResult'
 require './StringParser'
-require './DocParser'
 
 class ScannerTest < Test::Unit::TestCase
-  include DataDocument
-  include StringParser
-
   def testEmptyString
     # act
     sc = Scanner.new('')
 
     # assert
-    assesrt_equal([false, false], sc.popToken)
+    assert_equal([false, false], sc.popToken)
   end
 end
