e2cc49494f6a5c77ac8a8f72cf85c59fc03310da
kkikzk <kkikzk@gmail.com>
2014-04-12 10:31:43 +0900

    first commit

---
+++ test/DataDocumentTest.rb
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+require 'test/unit'
+require './ParseResult'
+require './StringParser'
+require './DocParser'
+
+class ScannerTest < Test::Unit::TestCase
+  include DataDocument
+  include StringParser
+
+end
